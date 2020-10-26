39d67f693422b2d85e325f9e7c3a1ec66e8103da
PaperFanz <petefan.7@gmail.com>
2020-10-25 23:40:02 -0500

    update scroll behavior

--- src/script.js
+++ src/script.js
@@ -1,8 +1,11 @@
 // script file for paperfanz.github.io
 var content = document.getElementById("content");
+var robotics = document.getElementById("robotics");
+var iot = document.getElementById("iot");
+var computing = document.getElementById("computing");
 
-function scrollToContent() {
-    content.scrollIntoView({
+function scrollToElement(e) {
+    e.scrollIntoView({
         block: 'center',
         behavior: 'smooth'
       });
