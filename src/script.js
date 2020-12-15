d6d5369212746c443bdca42eba94e312fa641d56
PaperFanz <petefan.7@gmail.com>
2020-12-15 10:57:43 -0600

    consolidate into 1 file

--- src/script.js
+++ src/script.js
@@ -1,12 +1,8 @@
 // script file for paperfanz.github.io
-var content = document.getElementById("content");
-var robotics = document.getElementById("robotics");
-var iot = document.getElementById("iot");
-var computing = document.getElementById("computing");
 
-function scrollToElement(e) {
-    e.scrollIntoView({
-        block: 'center',
-        behavior: 'smooth'
-      });
+function scrollToElement(eid) {
+  document.getElementById(eid).scrollIntoView({
+    block: 'center',
+    behavior: 'smooth'
+  });
 }
